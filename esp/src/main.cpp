f22ee937d885fd4beac2162ba313ab86dc7301f1
Polina Simonenko <rabarbrablad@gmail.com>
2023-08-18 14:01:15 +0200

    feat: added functionality to esp

--- esp/src/main.cpp
+++ esp/src/main.cpp
@@ -1,15 +1,22 @@
 #include <Arduino.h>
 #include <ESP8266WiFi.h>
 #include <WebSocketsClient.h>
+#include <FastLED.h>
+#include <ArduinoJson.h>
+#define NUM_LEDS 64
+
+const int   LedPin = 0;
 
 const char  *ssid = "rabarbra";
 const char  *pass = "12345678";
 
 const char  *api_host = "172.20.10.2";
 const int   api_port = 8000;
-const char  *api_path = "/ws/esp_1";
+const char  *api_path = "/ws_esp/123";
 
+CRGB leds[NUM_LEDS];
 WebSocketsClient    webSocket;
+StaticJsonDocument<200> doc;
 
 void    webSocketEvent(WStype_t type, uint8_t *payload, size_t length);
 
@@ -30,7 +37,8 @@ void    setup()
     Serial.println();
     Serial.print("Connected, IP address: ");
     Serial.println(WiFi.localIP());
-
+    FastLED.addLeds<WS2812B, LedPin, GRB>(leds, NUM_LEDS);
+    FastLED.setBrightness(50);
     webSocket.begin(api_host, api_port, api_path);
 	webSocket.onEvent(webSocketEvent);
 	webSocket.setReconnectInterval(5000);
@@ -41,9 +49,17 @@ void    loop()
     webSocket.loop();
 }
 
+void    doOp(uint8_t *payload)
+{
+    deserializeJson(doc, payload);
+    Serial.println(doc["op"].as<int>());
+    Serial.println(doc["msg"].as<String>());
+}
+
 void    webSocketEvent(WStype_t type, uint8_t * payload, size_t length)
 {
-	switch(type) {
+	switch(type)
+    {
 		case WStype_DISCONNECTED:
 			Serial.printf("[WSc] Disconnected!\n");
 			break;
@@ -54,6 +70,7 @@ void    webSocketEvent(WStype_t type, uint8_t * payload, size_t length)
 			break;
 		case WStype_TEXT:
 			Serial.printf("[WSc] get text: %s\n", payload);
+            doOp(payload);
 			break;
 		case WStype_BIN:
 			Serial.printf("[WSc] get binary length: %u\n", length);
