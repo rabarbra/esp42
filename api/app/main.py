f62c15b49582cb826c8b80d97b8eecbde9c26d4b
Polina Simonenko <rabarbrablad@gmail.com>
2023-08-18 10:35:24 +0200

    feat: created api

--- api/app/main.py
+++ api/app/main.py
@@ -1,97 +1,94 @@
-from typing import Dict
+from typing import Dict, List, Literal, Any
 from fastapi import FastAPI, WebSocket, WebSocketDisconnect
-from fastapi.responses import HTMLResponse
+from fastapi.responses import RedirectResponse
 
 app = FastAPI()
 
-html = """
-<!DOCTYPE html>
-<html>
-    <head>
-        <title>Chat</title>
-    </head>
-    <body>
-        <h1>WebSocket Chat</h1>
-        <h2>Your ID: <span id="ws-id"></span></h2>
-        <form action="" onsubmit="sendMessage(event)">
-            <input type="text" id="messageText" autocomplete="off"/>
-            <button>Send</button>
-        </form>
-        <ul id='messages'>
-        </ul>
-        <script>
-            var client_id = Date.now()
-            document.querySelector("#ws-id").textContent = client_id;
-            var ws = new WebSocket(`ws://172.20.10.2:8000/ws/${client_id}`);
-            ws.onmessage = function(event) {
-                var messages = document.getElementById('messages')
-                var message = document.createElement('li')
-                var content = document.createTextNode(event.data)
-                message.appendChild(content)
-                messages.appendChild(message)
-            };
-            function sendMessage(event) {
-                var input = document.getElementById("messageText")
-                ws.send(input.value)
-                input.value = ''
-                event.preventDefault()
-            }
-        </script>
-    </body>
-</html>
-"""
+frontend_endpoint = 'http://localhost:3000'
 
+class WsConnectionManager:
+    def __init__(self) -> None:
+        self.web: Dict[str, WebSocket] = {}
+        self.esp: Dict[str, WebSocket] = {}
+        self.web_esp: Dict[str, List[str]] = {}
 
-class ConnectionManager:
-	def __init__(self):
-		self.web_connections: Dict[str, WebSocket] = {}
-		self.esp_connections: Dict[str, WebSocket] = {}
+    async def connect(
+            self, websocket: WebSocket,
+            client_id: str, kind: Literal['web', 'esp']
+    ):
+        await websocket.accept()
+        if (kind == 'esp'):
+            self.esp.update({client_id: websocket})
+        else:
+            self.web.update({client_id: websocket})
+            self.web_esp.update({client_id: []})
 
-	async def connect(self, websocket: WebSocket, client_id: str):
-		await websocket.accept()
-		if (client_id.startswith("esp")):
-			self.esp_connections.update({client_id: websocket})
-		else:
-			self.web_connections.update({client_id: websocket})
+    async def disconnect(self, client_id: str, kind: Literal['web', 'esp']):
+        if (kind == 'esp' and client_id in self.esp):
+            del self.esp[client_id]
+        elif client_id in self.web:
+            del self.web[client_id]
+            del self.web_esp[client_id]
 
-	def disconnect(self, client_id: str):
-		if (client_id.startswith("esp") and client_id in self.esp_connections):
-			del self.esp_connections[client_id]
-		elif client_id in self.web_connections:
-			del self.web_connections[client_id]
+    async def connect_esp(self, web_id: str, esp_id: str):
+        if (web_id in self.web and esp_id in self.esp):
+            for _, val in self.web_esp.items():
+                if esp_id in val:
+                    return
+            self.web_esp[web_id].append(esp_id)
+            self.broadcast_web('taken')
 
-	async def send_personal_message(self, message: str, websocket: WebSocket):
-		await websocket.send_text(message)
+    async def send_response_message(self, message: str, websocket: WebSocket):
+        await websocket.send_text(message)
 
-	async def broadcast_web(self, message: str, client_id: str | None = None):
-		for id, connection in self.web_connections.items():
-			if not client_id or client_id != id:
-				await connection.send_text(message)
+    async def broadcast_web(self, message: str, client_id: str | None = None):
+    	for id, connection in self.web.items():
+            if not client_id or client_id != id:
+                await connection.send_text(message)
 
-	async def broadcast_esp(self, message: str):
-		for id, connection in self.esp_connections.items():
-			await connection.send_text(message)
+    async def broadcast_esp(self, message: str):
+        for _, connection in self.esp.items():
+            await connection.send_text(message)
 	
-	async def send_to_web(self, message: str, client_id: str):
-		await self.web_connections[client_id].send_text(message)
+    async def send_to_web(self, message: str, client_id: str):
+        await self.web[client_id].send_text(message)
 
+    async def send_to_esp(
+            self, message: Dict[str, Any],
+            web_id: str, esp_id: str
+    ):
+        if (web_id in self.web_esp and esp_id in self.web_esp[web_id]):
+            await self.esp[esp_id].send_json(message)
 
-manager = ConnectionManager()
-
+manager = WsConnectionManager()
 
 @app.get("/")
 async def get():
-    return HTMLResponse(html)
+    return RedirectResponse(frontend_endpoint)
 
-@app.websocket("/ws/{client_id}")
-async def websocket_endpoint(websocket: WebSocket, client_id: str):
-    await manager.connect(websocket, client_id)
+@app.websocket("/ws_web/{client_id}")
+async def ws_web_endpoint(websocket: WebSocket, client_id: str):
+    await manager.connect(websocket, client_id, 'web')
+    try:
+        while True:
+            data: Dict[str, str] = await websocket.receive_json()
+            if 'op' not in data:
+                continue
+            if (data['op'] == 'cnct_esp' and 'data' in data):
+                manager.connect_esp(client_id, data['data'].get('esp_id', None))
+            elif (data['op'] == 'msg_esp' and 'data' in data):
+                manager.send_to_esp(data['data'].get('json'), client_id, data['data'].get('esp_id'))
+    except WebSocketDisconnect:
+        manager.disconnect(client_id, 'web')
+
+@app.websocket("/ws_esp/{client_id}")
+async def ws_esp_endpoint(websocket: WebSocket, client_id: str):
+    await manager.connect(websocket, client_id, 'esp')
+    manager.broadcast_web('connected');
     try:
         while True:
             data = await websocket.receive_text()
-            await manager.send_personal_message(f"You wrote: {data}", websocket)
+            await manager.send_response_message(f"You wrote: {data}", websocket)
             await manager.broadcast_web(f"Client #{client_id} says: {data}", client_id)
-            await manager.broadcast_esp(f"Client #{client_id} says: {data}")
     except WebSocketDisconnect:
-        manager.disconnect(client_id)
-        await manager.broadcast_web(f"Client #{client_id} left the chat")
+        manager.disconnect(client_id, 'esp')
