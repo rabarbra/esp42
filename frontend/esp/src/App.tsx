ab5a0f645c4f30958fe575a60813152b0aa040a6
Polina Simonenko <rabarbrablad@gmail.com>
2023-08-21 12:47:48 +0200

    feat: reorganized frontend

--- frontend/esp/src/App.tsx
+++ frontend/esp/src/App.tsx
@@ -1,11 +1,11 @@
-import React            from 'react';
+import React                from 'react';
 import {
     AppShell
-}                       from '@mantine/core';
-import Main             from './pages/Main';
-import ThemeProvider    from './theme/ThemeProvider';
-import MainHeader       from './components/MainHeader';
-import ConnectionContext from './api/ConnectionContext';
+}                           from '@mantine/core';
+import Main                 from './pages/Main';
+import ThemeProvider        from './theme/ThemeProvider';
+import MainHeader           from './components/MainHeader';
+import ConnectionContext    from './api/ConnectionContext';
 
 function App() {
     const [connected, setConnected] = React.useState(false);
