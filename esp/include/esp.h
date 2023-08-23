a019c6cdab0cad6d1b4eae3d000c04ce59f125c7
Polina Simonenko <rabarbrablad@gmail.com>
2023-08-24 00:08:41 +0200

    feat: added life game

---
+++ esp/include/esp.h
@@ -0,0 +1,35 @@
+#ifndef ESP_H
+# define ESP_H
+# include <Arduino.h>
+# include <ArduinoJson.h>
+# include <FastLED.h>
+# include <ESP8266WiFi.h>
+# include <WebSocketsClient.h>
+
+// LED MATRIX SETTINGS
+# define NUM_LEDS 64
+# define BRIGHTNESS 60
+const int           LedPin = 0;
+CRGBArray<NUM_LEDS> leds;
+
+// WIFI SETTINGS
+const char  *ssid = "rabarbra";
+const char  *pass = "12345678";
+
+// WEBSOCKET SETTINGS
+const int   api_port = 80;
+const char  *api_host = "api.esp42.eu";
+//const int   api_port = 8080;
+//const char  *api_host = "172.20.10.2";
+const char  *api_path = "/ws_esp/123";
+
+WebSocketsClient            webSocket;
+
+StaticJsonDocument<2048>    doc;
+StaticJsonDocument<2048>    responseDoc;
+
+void    webSocketEvent(WStype_t type, uint8_t *payload, size_t length);
+void    connectionAnim();
+void    connectedAnim();
+void    life(unsigned int secs, bool send);
+#endif
--- esp/src/main.cpp
+++ esp/src/main.cpp
@@ -1,58 +1,4 @@
-#include <Arduino.h>
-#include <ArduinoJson.h>
-#include <FastLED.h>
-#include <ESP8266WiFi.h>
-#include <WebSocketsClient.h>
-
-// LED MATRIX SETTINGS
-#define NUM_LEDS 64
-#define BRIGHTNESS 60
-const int           LedPin = 0;
-CRGBArray<NUM_LEDS> leds;
-
-// WIFI SETTINGS
-const char  *ssid = "rabarbra";
-const char  *pass = "12345678";
-
-// WEBSOCKET SETTINGS
-const int   api_port = 80;
-const char  *api_host = "api.esp42.eu";
-//const int   api_port = 8080;
-//const char  *api_host = "172.20.10.2";
-const char  *api_path = "/ws_esp/123";
-
-WebSocketsClient            webSocket;
-StaticJsonDocument<2048>    doc;
-StaticJsonDocument<2048>    responseDoc;
-
-void    webSocketEvent(WStype_t type, uint8_t *payload, size_t length);
-
-void    connectiomAnim()
-{
-    static int led = 0;
-    static int color = 0x00FF00;
-
-    leds[led % 64] = color % 0x1000000;
-    FastLED.show();
-    color += 0x10;
-    led++;
-}
-
-void    connectedAnim()
-{
-    int ledArray[] = {17, 18, 19, 21, 23, 25, 27, 29, 30, 33, 35, 37, 38, 41, 42, 43, 45, 47};
-    int i = -1;
-    int len = 18;
-    int color = 0x00FF00;
-    
-    FastLED.clear();
-    FastLED.show();
-    while (++i < len)
-    {
-        leds[ledArray[i]] = color;
-        FastLED.show();
-    }
-}
+#include "esp.h"
 
 void    setup()
 {
@@ -70,7 +16,7 @@ void    setup()
     while (WiFi.status() != WL_CONNECTED)
     {
         delay(500);
-        connectiomAnim();
+        connectionAnim();
         Serial.print(WiFi.status());
     }
     connectedAnim();
@@ -101,6 +47,7 @@ void switchLed(int op, JsonObject obj)
     else
         leds[num] = 0x000000;
     FastLED.show();
+    FastLED.show();
 }
 
 void displayImg(JsonArray img)
@@ -108,8 +55,9 @@ void displayImg(JsonArray img)
     int i = 0;
     for(JsonVariant v : img) {
         leds[i++] = v.as<long>();
-        FastLED.show();
     }
+    FastLED.show();
+    FastLED.show();
 }
 
 void    sendImg()
@@ -135,11 +83,16 @@ void    doOp(uint8_t *payload)
     if (op_type == 0 || op_type == 1)
         switchLed(op_type, doc["msg"]);
     else if (op_type == 2)
+    {
         FastLED.clear(true);
+        FastLED.clear(true);
+    }
     else if (op_type == 3)
         displayImg(doc["arr"]);
     else if (op_type == 4)
         sendImg();
+    else if (op_type == 5)
+        life(100, true);
     else if (op_type == 9)
         webSocket.sendTXT("connected");
     doc.clear();
@@ -171,5 +124,102 @@ void    webSocketEvent(WStype_t type, uint8_t * payload, size_t length)
             break;
         default:
             break;
+    }
+}
+
+void    connectionAnim()
+{
+    static int led = 0;
+    static int color = 0x00FF00;
+
+    leds[led % 64] = color % 0x1000000;
+    FastLED.show();
+    color += 0x10;
+    led++;
+}
+
+void    connectedAnim()
+{
+    int ledArray[] = {17, 18, 19, 21, 23, 25, 27, 29, 30, 33, 35, 37, 38, 41, 42, 43, 45, 47};
+    int i = -1;
+    int len = 18;
+    int color = 0x00FF00;
+    
+    FastLED.clear();
+    FastLED.show();
+    while (++i < len)
+    {
+        leds[ledArray[i]] = color;
+        FastLED.show();
+    }
+}
+
+bool     isAlive(CRGB cell)
+{
+    if (cell.r || cell.g || cell.b)
+        return (true);
+    return (false);
+}
+
+int     calcNeighbours(CRGBArray<64> field, int pos)
+{
+    int res = 0;
+
+    if (pos - 8 >= 0 && isAlive(field[pos - 8]))
+        res++;
+    if (pos + 8 < 64 && isAlive(field[pos + 8]))
+        res++;
+    if (pos % 8 > 0)
+    {
+        if (isAlive(field[pos - 1]))
+            res++;
+        if (pos - 9 >= 0 && isAlive(field[pos - 9]))
+            res++;
+        if (pos + 7 < 64 && isAlive(field[pos + 7]))
+            res++;
+    }
+    if (pos % 8 < 7)
+    {
+        if (isAlive(field[pos + 1]))
+            res++;
+        if (pos + 9 < 64 && isAlive(field[pos + 9]))
+            res++;
+        if (pos - 7 >= 0 && isAlive(field[pos - 7]))
+            res++;
+    }
+    return (res);
+}
+
+void    life(unsigned int secs, bool send)
+{
+    CRGBArray<64>   buf;
+    int             neighbours;
+    int             clr = 0xFF0000;
+    long unsigned   begin = millis();
+
+    while (millis() < begin + secs * 1000)
+    {
+        buf = leds;
+        for(int i = 0; i < 64; i++)
+        {
+            neighbours = calcNeighbours(buf, i);
+            if (neighbours == 3)
+                leds[i] = clr;
+            else if (neighbours > 3 && !isAlive(buf[i])) 
+                leds[i] = clr;
+            else if (neighbours == 2 && isAlive(buf[i]))
+                leds[i] = clr;
+            else
+                leds[i] = 0;
+        }
+        FastLED.show();
+        FastLED.show();
+        if (send)
+        {
+            sendImg();
+            delay(200);
+        }
+        else
+            delay(500);
     }
 }
